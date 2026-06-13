c6006f400d49ea51cb5fbc0f26b3cb75f87fae57
Mark Chen <9006264+mark9804@users.noreply.github.com>
2026-06-13 22:52:36 +0900

    feat(misc): destruct function

--- hsi_utils/misc/__init__.py
+++ hsi_utils/misc/__init__.py
@@ -1,4 +1,22 @@
 from typing import Any
 
+
 def is_none(val: Any) -> bool:
     return val is None or (isinstance(val, str) and val.strip().lower() == "none")
+
+
+def destruct(d, *keys):
+    """
+    Return a tuple of values from a dictionary based on the given keys.
+
+    Examples:
+    >>> d = {"a": 1, "b": 2, "c": 3}
+    >>> a, b, c = destruct(d, "a", "b", "c")
+    >>> a
+    1
+    >>> b
+    2
+    >>> c
+    3
+    """
+    return (d[k] if k in d else None for k in keys)
